453f84d6ef380b051642e2148082b533da5b1f2b
DustinCabrera7 <dcabreradeveloper7@gmail.com>
2024-12-02 11:20:34 -0500

    funciones de suma resta multi

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,34 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms'; // Importa FormsModule
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'app-prueba';
+  numero1: number = 0;
+  numero2: number = 0;
+  resultado: number | string = '';
+
+  sumar(): void {
+    this.resultado = this.numero1 + this.numero2;
+  }
+
+  restar(): void {
+    this.resultado = this.numero1 - this.numero2;
+  }
+
+  multiplicar(): void {
+    this.resultado = this.numero1 * this.numero2;
+  }
+
+  limpiar(): void {
+    this.numero1 = 0;
+    this.numero2 = 0;
+    this.resultado = '';
+  }
 }
